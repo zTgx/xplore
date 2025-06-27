ce91d2a01b4de68231e83029c2d9a5919629db45
zTgx <747674262@qq.com>
2025-06-27 22:36:16 +0800

    remove dead code

--- src/api.rs
+++ src/api.rs
@@ -9,7 +9,6 @@ use {
     serde_json::Value,
 };
 
-// https://github.com/elizaOS/agent-twitter-client/blob/main/src/api.ts
 pub const BEARER_TOKEN: &str =
     "AAAAAAAAAAAAAAAAAAAAAFQODgEAAAAAVHTp76lzh3rFzcHbmHVvQxYYpTw%3DckAlMINMjmCwxUcaXbAN4XqJVdgMJaHqNOFgPMK0zN1qLqLQCF";
 
